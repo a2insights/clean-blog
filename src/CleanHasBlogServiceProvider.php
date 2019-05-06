156c2ce806c524bb656b142bd1e27e3a09d80e0b
Atiladanvi <atila.danvi@outlook.com>
2019-05-06 15:14:15 -0300

    first commit

---
+++ src/CleanHasBlogServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CleanHasBlog;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class CleanHasBlogServiceProvider
+ * @package MaterialAdmin
+ */
+class CleanHasBlogServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the resources path
+     * @void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/public/assets' => public_path('vendor/clean/assets'),
+        ], 'clean/assets');
+
+        $this->publishes([
+            __DIR__.'/resources/views' => resource_path('views/vendor/clean'),
+        ], 'clean/views');
+    }
+
+    /**
+     * Register resources
+     * @void
+     */
+    public function register()
+    {
+
+    }
+}
